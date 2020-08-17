99d074f0486f10c83700f470f985cfb0dff24bc3
Koushik15042000 <kingskoushiks19992gmail.com>
2020-08-17 20:29:38 +0530

    Day - 4 Commit

---
+++ app/src/main/java/com/example/umang/RegUser.java
@@ -0,0 +1,40 @@
+package com.example.umang;
+
+public class RegUser {
+
+    String name, email, phoneNumber;
+
+    public RegUser(String name, String email, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public RegUser(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
--- app/src/main/java/com/example/umang/RegistrationActivity.java
+++ app/src/main/java/com/example/umang/RegistrationActivity.java
@@ -16,26 +16,35 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    EditText edtuserName, edtemailId, edtpassword, edtconfirmPassword;
+    EditText edtuserName, edtemailId, edtpassword, edtconfirmPassword, edtPhoneNumber;
     Button btnregister;
-    String name, email, password, confirmpassword;
+    String name, email, password, confirmpassword, phoneNumber;
 
     FirebaseAuth mAuth;
+
+    DatabaseReference myRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+
         edtuserName = findViewById(R.id.editTextUserNameField);
         edtemailId = findViewById(R.id.editTextEmailIdField);
         edtpassword = findViewById(R.id.editTextPasswordField);
         edtconfirmPassword = findViewById(R.id.editTextConfirmPasswordField);
+        edtPhoneNumber = findViewById(R.id.editTextPhoneNumberField);
 
         mAuth = FirebaseAuth.getInstance();
+
+        myRef = FirebaseDatabase.getInstance().getReference().child("User_Data");
 
         btnregister = findViewById(R.id.buttonRegister);
 
@@ -47,6 +56,8 @@ public class RegistrationActivity extends AppCompatActivity {
                 email = edtemailId.getText().toString().trim();
                 password = edtpassword.getText().toString().trim();
                 confirmpassword = edtconfirmPassword.getText().toString().trim();
+                phoneNumber = edtPhoneNumber.getText().toString().trim();
+
 
                 if(name.isEmpty()) {
                     Toast.makeText(RegistrationActivity.this, "Please enter Name", Toast.LENGTH_SHORT).show();
@@ -58,12 +69,31 @@ public class RegistrationActivity extends AppCompatActivity {
                     Toast.makeText(RegistrationActivity.this, "Please enter COnfirm pasword", Toast.LENGTH_SHORT).show();
                 } else if(!password.equals(confirmpassword)) {
                     Toast.makeText(RegistrationActivity.this, "Password and Conf Password should same", Toast.LENGTH_SHORT).show();
-                } else {
+                }  else if(phoneNumber.isEmpty() && phoneNumber.length() < 10) {
+                    Toast.makeText(RegistrationActivity.this, "Please enter a Valid Phone Number", Toast.LENGTH_SHORT).show();
+                }else {
                     next();
+                    userReg();
                 }
 
             }
         });
+
+    }
+
+    private void userReg() {
+
+        RegUser regUser = new RegUser(name, email, phoneNumber);
+
+        String key = myRef.push().getKey();
+
+        // myRef is Pointing to User_Data Child
+        // Child will be created for User_Data with Id
+        myRef.child(key).setValue(regUser);
+
+        Toast.makeText(this, "User Registered Successfully", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+        startActivity(intent);
 
     }
 
@@ -82,4 +112,6 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
